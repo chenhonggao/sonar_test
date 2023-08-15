2dd228a125d91ba849a23ffa284c216a57e61767
chenhonggao <416742651@qq.com>
2023-08-15 14:57:38 +0800

    second

--- sonar.sh
+++ sonar.sh
@@ -6,4 +6,5 @@ mvn clean verify sonar:sonar \
     -Dsonar.token=bf8d6b8ec446e7769fab78000623c9a84b9e9059 \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=chenhonggao \
-    -Dsonar.projectKey=chenhonggao_sonar_test
+    -Dsonar.projectKey=chenhonggao_sonar_test \
+    -Dsonar.branch.name=sonar
