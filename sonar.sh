873e6e8311cc1f35b1e010802f8dfd123046cbd4
chenhonggao <416742651@qq.com>
2023-08-15 15:33:30 +0800

    xx

--- sonar.sh
+++ sonar.sh
@@ -3,9 +3,9 @@
 export JAVA_HOME=/Users/honggao.chen/Downloads/jdk-17.0.8.jdk/Contents/Home
 
 mvn clean verify sonar:sonar -Pcoverage \
+#    -Dsonar.exclusions=pom.xml,src/main/java/cn/simple/WorldUtils.java \
     -Dsonar.token=bf8d6b8ec446e7769fab78000623c9a84b9e9059 \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=chenhonggao \
     -Dsonar.projectKey=chenhonggao_sonar_test \
-    -Dsonar.branch.name=sonar \
-    -Dsonar.exclusions=pom.xml,src/main/java/cn/simple/WorldUtils.java
+    -Dsonar.branch.name=sonar
