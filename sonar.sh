3f7bc811a4691f2381920a46bb60a60e2eb8b7a6
chenhonggao <416742651@qq.com>
2023-08-15 14:50:36 +0800

    add sonar

---
+++ sonar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export JAVA_HOME=/Users/honggao.chen/Downloads/jdk-17.0.8.jdk/Contents/Home
+
+mvn clean verify sonar:sonar \
+    -Dsonar.token=bf8d6b8ec446e7769fab78000623c9a84b9e9059 \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.organization=chenhonggao \
+    -Dsonar.projectKey=chenhonggao_sonar_test
