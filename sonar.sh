43562cf491026555288deaf9750a9b7d11921274
chenhonggao <416742651@qq.com>
2023-08-16 17:14:22 +0800

    second

--- sonar.sh
+++ sonar.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-mvn clean verify -Dmaven.test.failure.ignore=true
+
+mvn -T 1C clean org.jacoco:jacoco-maven-plugin:prepare-agent test -Dmaven.test.failure.ignore=true
 
 export JAVA_HOME=/Users/honggao.chen/Downloads/jdk-17.0.8.jdk/Contents/Home
 export JAVA_HOME=/Users/honggao.chen/Downloads/jdk-11.0.16.1.jdk/Contents/Home
 
-mvn sonar:sonar  \
+mvn -T 1C sonar:sonar \
     -Dsonar.token=bf8d6b8ec446e7769fab78000623c9a84b9e9059 \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=chenhonggao \
