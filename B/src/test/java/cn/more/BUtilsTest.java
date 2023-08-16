7db2cd449e66c5f222156d0d598d848b7d448c32
chenhonggao <416742651@qq.com>
2023-08-16 17:08:20 +0800

    zj

--- B/src/test/java/cn/more/BUtilsTest.java
+++ B/src/test/java/cn/more/BUtilsTest.java
@@ -2,11 +2,17 @@ package cn.more;
 
 
 import org.junit.Test;
+import org.springframework.util.Assert;
 
 public class BUtilsTest {
 
     @Test
+    public void plus() {
+        BUtils.plus(1,2);
+    }
+
+    @Test
     public void minus() {
-        BUtils.minus(2,1);
+        BUtils.minus(1,2);
     }
 }
--- sonar.sh
+++ sonar.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
+mvn clean verify -Dmaven.test.failure.ignore=true
 
 export JAVA_HOME=/Users/honggao.chen/Downloads/jdk-17.0.8.jdk/Contents/Home
+export JAVA_HOME=/Users/honggao.chen/Downloads/jdk-11.0.16.1.jdk/Contents/Home
 
-mvn clean verify sonar:sonar -Pcoverage \
+mvn sonar:sonar  \
     -Dsonar.token=bf8d6b8ec446e7769fab78000623c9a84b9e9059 \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=chenhonggao \
     -Dsonar.projectKey=chenhonggao_sonar_test \
     -Dsonar.branch.name=sonar
-
-
-
-#    -Dsonar.exclusions=pom.xml,src/main/java/cn/simple/WorldUtils.java \
-#mvn clean verify sonar:sonar -Pcoverage \
